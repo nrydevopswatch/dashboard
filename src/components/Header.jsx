238def428ff2d7ba695beae30a4ae3ef657e164e
Ry Bruscoe <53944812+nrydevopswatch@users.noreply.github.com>
2020-01-28 17:16:17 -0500

    Working on SVG logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ import { useHistory } from "react-router-dom";
 import { toggleRightPanel } from "../redux/layout/layout.action";
 import { toggleTheme } from "../redux/theme/theme.action";
 import { Box, Button, Heading } from "grommet";
-import { Notification, Info } from "grommet-icons";
+import { Notification, Info, Blank } from "grommet-icons";
 import styled from "styled-components";
 
 // Component Import
-import TabMenu from "./TabMenu"
+import TabMenu from "./TabMenu";
 
 const StyledHeading = styled(Heading)`
   &:hover {
@@ -30,20 +30,24 @@ const Header = () => {
       elevation="medium"
       style={{ zIndex: "1" }}
     >
-    <TabMenu
-      label="Menu"
-      align={{ top: "bottom", left: "right" }}
-      items={[
-        { label: 'First Action', onClick: () => {} },
-        { label: 'Second Action', onClick: () => {} },
-      ]}
-    />
+      <Blank />
+      <TabMenu
+        label="Menu"
+        align={{ top: "bottom", left: "right" }}
+        items={[
+          { label: "First Action", onClick: () => {} },
+          { label: "Second Action", onClick: () => {} }
+        ]}
+      />
       <StyledHeading level="3" margin="none" onClick={() => history.push("/")}>
         Dashboard
       </StyledHeading>
       <Box direction="row">
         <Button icon={<Info />} onClick={() => dispatch(toggleTheme())} />
-        <Button icon={<Notification />} onClick={() => dispatch(toggleRightPanel())} />
+        <Button
+          icon={<Notification />}
+          onClick={() => dispatch(toggleRightPanel())}
+        />
       </Box>
     </Box>
   );
